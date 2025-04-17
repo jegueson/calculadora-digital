c99deacc605d43c035bd4ba2923bdd764f18e2fa
Gabriel Candido <gabriel.candido@gmail.com>
2025-04-17 14:16:56 +0200

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  output: 'export',
+  basePath: '/calculadora-digital',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
