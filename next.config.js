2df83392fa77ed8d2b98a7f976ab18abbea64127
jegueson <gabriel.candido@gmail.com>
2025-04-17 19:26:59 +0200

    fix: update routing configuration for static export

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   output: 'export',
+  distDir: 'build',
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dev.calculadora-digital.com.br' : '',
+  trailingSlash: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
